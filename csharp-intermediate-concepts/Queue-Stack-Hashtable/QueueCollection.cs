a8ffb7efc80ae7542d74dbd4027d5843689dd130
Imanebln <boulouane.imane@gmail.com>
2022-09-10 00:08:52 +0100

    added Queue and Stack Concepts

---
+++ csharp-intermediate-concepts/Queue-Stack-Hashtable/QueueCollection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Stack_Hashtable
+{
+    public class QueueCollection
+    {
+        public static void Main(string[] args)
+        {
+            //By using System.Collection.Generic namespace:
+            Queue<int>  intCollection = new();
+
+            //And by using System.Collection namespace:
+            Queue queueCollection = new Queue();
+
+            //If we declare an object by providing a type (in our example an int), we can store only integer numbers inside. On the other hand, if we use the second example we can store different data types in a collection because it stores objects.
+
+            //Methods and Properties
+            queueCollection.Enqueue(54);
+            queueCollection.Enqueue("Imane");
+            queueCollection.Enqueue(54.01);
+
+            foreach(var item in queueCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+            //When we want to  remove an element at the beginning of the collection and return it, we are going to use the Dequeue method:
+            int number = Convert.ToInt32(queueCollection.Dequeue());
+            Console.WriteLine($"Removed element is: {number}");
+            Console.WriteLine();
+
+            foreach(var item in queueCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+            //The Peek method returns the element at the beginning of the collection but does not remove it:
+            Queue queueCollection2 = new Queue();
+            queueCollection2.Enqueue(54);
+            queueCollection2.Enqueue("John");
+            queueCollection2.Enqueue(54.10);
+
+            int peekNumber = Convert.ToInt32(queueCollection2.Peek());
+            Console.WriteLine($"Returned element is: {peekNumber}");
+            Console.WriteLine();
+
+            //The Clear method removes all the elements from a collection.
+            queueCollection.Clear();
+
+            //If we want to check how many elements we have inside a collection, we can use the Count property:
+            Console.WriteLine(queueCollection.Count);
+
+        }
+    }
+}
---
+++ csharp-intermediate-concepts/Queue-Stack-Hashtable/StackCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Stack_Hashtable
+{
+    public class StackCollection
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            Stack<int> stackInt = new Stack<int>();
+
+            // Methods and Properties
+            //The Push method inserts an object at the top of the collection:
+            Stack stack1 = new Stack();
+            stack1.Push(328);
+            stack1.Push("Fifty Five");
+            stack1.Push(124.80);
+
+            foreach(var item in stack1)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Pop removes the element which was included last in a collection and returns it:
+            Stack stackCollection2 = new Stack();
+            stackCollection2.Push(328);
+            stackCollection2.Push("Fifty Five");
+            stackCollection2.Push(124.87);
+
+            double number = Convert.ToDouble(stackCollection2.Pop());
+            Console.WriteLine($"Element removed from collection is: {number}");
+
+            foreach(var item in stackCollection2)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Peek returns an object ready to exit the collection, but it doesn’t remove it:
+            Stack stackCollection3 = new Stack();
+            stackCollection3.Push(328);
+            stackCollection3.Push("Fifty Five");
+            stackCollection3.Push(124.87);
+
+            double number1 = Convert.ToDouble(stackCollection3.Peek());
+
+            Console.WriteLine($"Element returned from a collection is: {number1}");
+            foreach (var item in stackCollection3)
+            {
+                Console.WriteLine(item);
+            }
+
+            //To remove all objects from a collection, we use the Clear method.
+            stackCollection2.Clear();
+
+            //If we want to count the number of elements, we use the Count property:
+            Console.WriteLine(stackCollection2.Count);
+
+
+        }
+    }
+}
