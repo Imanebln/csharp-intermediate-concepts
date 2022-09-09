d6330ce857a88ce9464061c4f8874d6b47765e96
Imanebln <boulouane.imane@gmail.com>
2022-09-09 23:06:52 +0100

    added List and Dictionary Concepts

---
+++ csharp-intermediate-concepts/List-and-Dictionary/Dictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_and_Dictionary
+{
+    public class Dictionary
+    {
+        public static void Main(string[] args)
+        {
+            //Dictionary has several constructors we can use to instantiate objects:
+
+            Dictionary<string, int> dictExample = new Dictionary<string, int>();
+
+            Dictionary<string, int> dictExample1 = new Dictionary<string, int>(5); //to set initial size
+
+            Dictionary<string, int> dictExample2 = new Dictionary<string, int>(dictExample1); //accepts all the elements from created Key-Value collection
+
+            //Methods and Properties
+            //The Add method adds the key-value pair inside a collection:
+            dictExample.Add("First", 100);
+            dictExample.Add("Second", 200);
+            dictExample.Add("Third", 300);
+
+            foreach(var item in dictExample)
+            {
+                Console.WriteLine(dictExample[item.Key]);
+            }
+
+            //Remove removes the key-value pair from a collection based on the specified key:
+            dictExample.Remove("Second");
+
+            //ContainsKey determines if a collection contains a specific key.
+            //ContainsValue determines if a collection contains a specific value:
+            if (dictExample.ContainsKey("First"))
+            {
+                Console.WriteLine("It Contains key");
+            }
+
+            if (dictExample.ContainsValue(300))
+            {
+                Console.WriteLine("It contains value");
+            }
+
+            //The Clear method removes all key-value pairs from a collection:
+            //dictExample.Clear();
+
+            //If we want to count all of our elements inside a collection, we can use the Count property. If we want to get a collection of containing Keys or containing Values from a dictionary, we can use the Keys and Values properties:
+            Console.WriteLine(dictExample.Count);
+
+            foreach(var key in dictExample.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            foreach(var value in dictExample.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+
+        }
+    }
+}
---
+++ csharp-intermediate-concepts/List-and-Dictionary/List.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_and_Dictionary
+{
+    public class List
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers= new();
+            numbers.Add(34);
+            numbers.Add(58);
+            numbers.Add(69);
+
+            int[] nums = new int[5] {1,2,3,4,5};
+            // AddRange adds the elements of the specified collection to the end of a list:
+            numbers.AddRange(nums);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //We can check how many elements a list has by using the Count property:
+            Console.WriteLine(numbers.Count);
+
+            //Contains determines whether an element exists in the list:
+            if (numbers.Contains(34))
+            {
+                Console.WriteLine("The number 34 exists in a list");
+            }
+
+            //The IndexOf method returns the position of an element as an integer number. If an element couldn’t be found, this method returns -1:
+            int index;
+            if((index = numbers.IndexOf(58)) != -1)
+            {
+                Console.WriteLine($"The number 58 is on the index: {index}");
+            }
+            //LastIndexOf is similar to a previous method except it returns a last occurrence of the element.
+
+            //CopyTo method copies the entire collection to a compatible array, starting from the beginning of that array:
+            int[] copyArray = new int[10];
+            numbers.CopyTo(copyArray);
+            foreach(int copyNumber in copyArray)
+            {
+                Console.WriteLine(copyNumber);
+            }
+
+            //The Remove method removes the first occurrence of a specific element from the list:
+            numbers.Remove(69);
+
+            //The Clear method clears all the elements from a list:
+            numbers.Clear();
+
+            
+
+
+        }
+    }
+}
