6d3ff816ea4b48cabbd022cc5d6994b1fd60907e
Imanebln <boulouane.imane@gmail.com>
2022-09-28 01:09:37 +0100

    Added Anonymous and Nullable Types Concepts

---
+++ csharp-intermediate-concepts/Anonymous-and-Nullable-Types/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_and_Nullable_Types
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAnonymousObj = new { Name = "Imane", Age = 23 };
+
+            Console.WriteLine($"The name of myAnonymousObject is {myAnonymousObj.Name}, the age is {myAnonymousObj.Age}");
+
+            int? number = null;
+            number = 234;//comment this line to print out another result
+
+            // The nullable types expose a few properties which can come in handy while working on our projects.
+            // The HasValue property indicates whether a nullable type contains a value or it is a null.
+            // The Value property enables us to retrieve the value of the nullable type if it is not null:
+
+            if (number.HasValue)
+            {
+                Console.WriteLine(number.Value);
+            }
+            else
+            {
+                Console.WriteLine("number is null");
+            }
+        }
+    }
+}
