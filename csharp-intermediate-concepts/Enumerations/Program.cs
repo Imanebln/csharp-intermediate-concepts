b5d6c29a136b0172dff01221e9e82d7d0fa8ba89
Imanebln <boulouane.imane@gmail.com>
2022-09-29 00:34:22 +0100

    added Enumerations Concept

---
+++ csharp-intermediate-concepts/Enumerations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysInWeek monday = DaysInWeek.Monday;
+
+            Console.WriteLine(monday); // It is going to print out Monday
+            Console.WriteLine((int)monday); //prints out the 0
+            Console.ReadKey();
+        }
+    }
+    public enum DaysInWeek: short
+    {
+        Monday = 10,
+        Tuesday = 20,
+        Wednesday = 35,
+        Thursday = 48,
+        Friday = 74,
+        Saturday = 12,
+        Sunday = 154
+    }
+}
