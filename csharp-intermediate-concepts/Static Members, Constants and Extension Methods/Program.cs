9ab493ca2813b216e6221e0916fb844149b69009
Imanebln <boulouane.imane@gmail.com>
2022-10-06 22:25:19 +0100

    added Static Members, Constants and Extension Methods Concepts

---
+++ csharp-intermediate-concepts/Static Members, Constants and Extension Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Members__Constants_and_Extension_Methods
+{
+    //The static class can contain only the static members.
+    //Its purpose is to act as a holder for the utility methods and fields. 
+    public static class TestClass
+    {
+        private static int number;
+
+        static TestClass()
+        {
+            number = 54;
+        }
+    }
+    public class Program
+    {
+        //Creating a Field by Using the Const Keyword
+        public const int constNumber = 15;
+        static void Main(string[] args)
+        {
+            string word = "football".FirstLetterUpperCase();
+
+            Console.WriteLine(word.GetType()); //string type
+            Console.WriteLine(word);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp-intermediate-concepts/Static Members, Constants and Extension Methods/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Members__Constants_and_Extension_Methods
+{
+    public static class StringExtensions
+    {
+        public static string FirstLetterUpperCase(this string word)
+        {
+            char letter = Char.ToUpper(word[0]);
+            string remaining = word.Substring(1);
+            return letter + remaining;
+        }
+    }
+}
